5bfcc94369141f45627ab5ff0fc2b6d8b75b63d2
bivav <bivavraj@gmail.com>
2024-02-29 16:47:18 +0000

    Created passwords.json.enc and user input for password

--- src/lib.rs
+++ src/lib.rs
@@ -27,116 +27,171 @@ impl AppConfig {
     pub fn am_i_root() -> bool {
         unistd::geteuid().is_root()
     }
-}
 
-pub fn get_password_file_path() -> Result<PathBuf> {
-    let home_dir = dirs::home_dir().context("Home directory not found")?;
-    let app_dir = home_dir.join(".rustic_key_vault");
-    let password_file = app_dir.join("passwords.json.enc");
+    pub fn get_app_dir() -> Result<PathBuf> {
+        let home_dir = dirs::home_dir().context("Home directory not found")?;
+        let app_dir = home_dir.join(".rustic_key_vault");
 
-    // if !app_dir.exists() {
-    //     fs::create_dir_all(&app_dir).context("Failed to create app directory")?;
-    //     let permission = fs::Permissions::from_mode(0o700);
-    //     fs::set_permissions(&app_dir, permission)
-    //         .context("Failed to set permission on app directory")?;
-    // }
-
-    Ok(password_file)
-}
-
-fn get_app_dir() -> Result<PathBuf> {
-    let home_dir = dirs::home_dir().context("Home directory not found")?;
-    let app_dir = home_dir.join(".rustic_key_vault");
-
-    if !app_dir.exists() {
-        fs::create_dir_all(&app_dir).context("Failed to create app directory")?;
-        let permission = fs::Permissions::from_mode(0o700);
-        fs::set_permissions(&app_dir, permission)
-            .context("Failed to set permission on app directory")?;
-    }
-
-    Ok(app_dir)
-}
-
-pub fn get_passwords() -> Result<PathBuf> {
-    let password_file_path = get_password_file_path()?;
-
-    OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(&password_file_path)
-        .context("Failed to create password file")?;
-
-    fs::set_permissions(&password_file_path, fs::Permissions::from_mode(0o600))
-        .context("Failed to set permission on password file")?;
-
-    println!("Password file path: {:?}", password_file_path);
-
-    Ok(password_file_path)
-}
-
-pub fn create_or_get_hash_file() -> Result<PathBuf> {
-    let app_dir = get_app_dir()?;
-
-    let hash_file = app_dir.join("master.hash");
-
-    OpenOptions::new()
-        .write(true)
-        .create(true)
-        .read(true)
-        .open(&hash_file)
-        .context("Failed to create password file")?;
-
-    fs::set_permissions(&hash_file, fs::Permissions::from_mode(0o600))
-        .context("Failed to set permission on hash file")?;
-
-    println!("Hash file path: {:?}", hash_file);
-
-    Ok(hash_file)
-}
-
-pub fn create_or_get_master_password() -> Result<String> {
-    let hash_file = create_or_get_hash_file()?;
-
-    let hash = fs::read_to_string(&hash_file).context("Failed to read hash file")?;
-
-    return if hash.trim().is_empty() {
-        println!("Creating a master password");
-        let password = password_prompt()?;
-
-        if password.trim().is_empty() {
-            return Err(anyhow::anyhow!("Password cannot be empty"));
+        if !app_dir.exists() {
+            fs::create_dir_all(&app_dir).context("Failed to create app directory")?;
+            let permission = fs::Permissions::from_mode(0o700);
+            fs::set_permissions(&app_dir, permission)
+                .context("Failed to set permission on app directory")?;
         }
 
-        let salt = SaltString::generate(&mut OsRng);
+        Ok(app_dir)
+    }
 
-        // 'Argon2id' with default parameters
-        let argon2 = Argon2::default();
+    pub fn get_password_file_path() -> Result<PathBuf> {
+        let home_dir = dirs::home_dir().context("Home directory not found")?;
+        let app_dir = home_dir.join(".rustic_key_vault");
+        let password_file = app_dir.join("passwords.json.enc");
 
-        // Hash password to PHC string ($argon2id$v=19$...)
-        let password_hash = argon2
-            .hash_password(password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+        // if !app_dir.exists() {
+        //     fs::create_dir_all(&app_dir).context("Failed to create app directory")?;
+        //     let permission = fs::Permissions::from_mode(0o700);
+        //     fs::set_permissions(&app_dir, permission)
+        //         .context("Failed to set permission on app directory")?;
+        // }
 
-        // Write the hash to the hash file
-        fs::write(&hash_file, &password_hash).context("Failed to write hash to file")?;
-        println!("Master password is set. Please enter the password to continue.");
-        Ok(password_hash)
-    } else {
-        // println!("Master password already exists");
-        Ok(hash)
-    };
+        Ok(password_file)
+    }
+
+    pub fn create_or_get_hash_file() -> Result<PathBuf> {
+        let app_dir = Self::get_app_dir()?;
+        let hash_file = app_dir.join("master.hash");
+
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open(&hash_file)
+            .context("Failed to create master hash file")?;
+
+        fs::set_permissions(&hash_file, fs::Permissions::from_mode(0o600))
+            .context("Failed to set permission on hash file")?;
+
+        println!("Hash file path: {:?}", hash_file);
+
+        Ok(hash_file)
+    }
+
+    pub fn create_or_get_password_file() -> Result<PathBuf> {
+        let app_dir = Self::get_app_dir()?;
+
+        let password_file = app_dir.join("passwords.json.enc");
+
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open(&password_file)
+            .context("Failed to create password file")?;
+
+        fs::set_permissions(&password_file, fs::Permissions::from_mode(0o600))
+            .context("Failed to set permission on password file")?;
+
+        Ok(password_file)
+    }
+
+    pub fn create_or_get_master_password() -> Result<(String, PathBuf)> {
+        let hash_file = Self::create_or_get_hash_file()?;
+
+        let hash = fs::read_to_string(&hash_file).context("Failed to read hash file")?;
+
+        return if hash.trim().is_empty() {
+            println!("Creating a master password");
+            let password = Self::password_prompt()?;
+
+            if password.trim().is_empty() {
+                return Err(anyhow::anyhow!("Password cannot be empty"));
+            }
+
+            let salt = SaltString::generate(&mut OsRng);
+
+            // 'Argon2id' with default parameters
+            let argon2 = Argon2::default();
+
+            // Hash password to PHC string ($argon2id$v=19$...)
+            let password_hash = argon2
+                .hash_password(password.as_bytes(), &salt)
+                .unwrap()
+                .to_string();
+
+            // create passwords.json.enc file
+            let password_file = Self::create_or_get_password_file()?;
+
+            // Write the hash to the hash file
+            fs::write(&hash_file, &password_hash).context("Failed to write hash to file")?;
+            println!("Master password is set. Please enter the master password to continue.");
+
+            Ok((password_hash, password_file))
+        } else {
+            // Get passwords.json.enc file
+            let password_file = Self::create_or_get_password_file()?;
+
+            Ok((hash, password_file))
+        };
+    }
+
+    pub fn password_prompt() -> Result<String> {
+        let password = rpassword::prompt_password("Master Password: ")?;
+        Ok(password)
+    }
+
+    pub fn match_password(password: &[u8], password_hash: &str) -> Result<bool> {
+        let parsed_hash = PasswordHash::new(&password_hash).map_err(|e| anyhow!(e))?;
+        Ok(Argon2::default()
+            .verify_password(password, &parsed_hash)
+            .is_ok())
+    }
+
+    // read_password_file
+    pub fn read_password_file(password_file: &PathBuf) -> Result<()> {
+        let file = OpenOptions::new()
+            .read(true)
+            .open(password_file)
+            .context("Failed to open password file")?;
+
+        let reader = std::io::BufReader::new(file);
+        let passwords: Vec<String> =
+            serde_json::from_reader(reader).context("Failed to read password file")?;
+        println!("Passwords: {:?}", passwords);
+        Ok(())
+    }
+
+    // match the choice from user
+    pub fn match_choice(choice: &str) -> Result<()> {
+        match choice.trim() {
+            "1" => {
+                println!("Add a new password");
+            }
+            "2" => {
+                println!("Get a password");
+            }
+            "3" => {
+                println!("Update a password");
+            }
+            "4" => {
+                println!("Delete a password");
+            }
+            "5" => {
+                println!("List all passwords");
+            }
+            "6" => {
+                println!("Exit");
+            }
+            _ => {
+                println!("Invalid choice");
+            }
+        }
+        Ok(())
+    }
+
 }
 
-pub fn password_prompt() -> Result<String> {
-    let password = rpassword::prompt_password("Master Password: ")?;
-    Ok(password)
-}
-
-pub fn match_password(password: &[u8], password_hash: &str) -> Result<bool> {
-    let parsed_hash = PasswordHash::new(&password_hash).map_err(|e| anyhow!(e))?;
-    Ok(Argon2::default()
-        .verify_password(password, &parsed_hash)
-        .is_ok())
+struct Password {
+    username: String,
+    password: String,
+    domain: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use anyhow::Result;
+use std::io::Write;
+
+use anyhow::{Context, Result};
 use clap::{Arg, Command};
 
-use rustic_key_vault::{match_password, password_prompt, AppConfig};
+use rustic_key_vault::*;
 
 fn main() -> Result<()> {
     let matches = Command::new("rustic_key_vault")
@@ -31,23 +33,64 @@ fn main() -> Result<()> {
             let config = AppConfig::from_matches(sub_matches);
             println!("Logging in as: {}", config.username);
 
-            let expected_password = rustic_key_vault::create_or_get_master_password()?;
-            // println!("Expected password: {}", expected_password);
+            let (expected_password, password_file) = AppConfig::create_or_get_master_password()?;
 
+            let mut count = 0;
+            let mut matched_password = false;
 
+            // while count < 3, ask for password prompt
+            while count < 3 && !matched_password {
+                let password = AppConfig::password_prompt()?;
 
-            let password = password_prompt()?;
-
-            if password.trim().is_empty() {
-                return Err(anyhow::anyhow!("Password cannot be empty"));
+                if password.trim().is_empty() {
+                    return Err(anyhow::anyhow!("Password cannot be empty"));
+                }
+                let val = AppConfig::match_password(&password.as_bytes(), &expected_password)?;
+                if val {
+                    matched_password = true;
+                } else {
+                    println!("Password does not match. Please try again.");
+                    count += 1;
+                }
             }
 
-            let val = match_password(&password.as_bytes(), &expected_password)?;
+            if matched_password {
+                println!("Login successful");
 
-            println!("Password match: {}", val);
+                // List the options for user to choose from
+                println!("Choose an option:");
 
-            // Check for password file
-            // let _password_file = rustic_key_vault::get_passwords()?;
+                println!("Search for a password using domain name");
+                write!(std::io::stdout(), "Enter domain name: ")?;
+                std::io::stdout().flush()?;
+                let mut domain = String::new();
+                std::io::stdin()
+                    .read_line(&mut domain)
+                    .context("Failed to read domain name")?;
+
+                println!("Domain: {}", domain.trim());
+
+                println!("1. Add a new password");
+                println!("2. Get a password");
+                println!("3. Update a password");
+                println!("4. Delete a password");
+                println!("5. List all passwords");
+                println!("6. Exit");
+
+                // read the password file
+                let passwords = AppConfig::read_password_file(&password_file)?;
+
+                // get the user's choice
+                let mut choice = String::new();
+                std::io::stdin()
+                    .read_line(&mut choice)
+                    .context("Failed to read user's choice")?;
+
+                // match the choice
+                let val = AppConfig::match_choice(&choice)?;
+            } else {
+                println!("Login failed");
+            }
         }
         Some(("reset", reset_matches)) => {
             if reset_matches.subcommand_matches("master").is_some() {
